2e29a9cfd773b75f0f4243579d05033b998f7040
mitchell <kim3603di@gmail.com>
2020-10-15 11:31:33 +0900

    rebulid

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/policPhotographer" : "/",
+    process.env.NODE_ENV === "production" ? "/polic-photographer" : "/",
   outputDir: "docs",
 };
